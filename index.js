920fad9e1f65d1ff93eb4ede43f919c40bd474dd
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-08-19 00:19:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record){
+	let oneRecord = record.find((r)=> r.result == "W");
+    if(oneRecord){
+        return oneRecord.year;
+    }else{
+        return oneRecord;
+    }
+}
